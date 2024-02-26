e795cbe4099379be1691483ded997c78468ad2de
Aman Prasad <aprasad2707@gmail.com>
2024-02-26 20:53:00 +0530

    adding popup

---
+++ Popup/script.js
@@ -0,0 +1,8 @@
+const popup = document.getElementById('popup');
+
+const openPopup = () => {
+  popup.classList.add('open-popup');
+}
+const closePopup = () => {
+popup.classList.remove('open-popup');
+}
